3eceabb7ff75552853d80534aa94e9f482251120
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2023-10-15 12:51:42 -0300

    fix cards

--- src/components/photoGallery/PhotoGallery.js
+++ src/components/photoGallery/PhotoGallery.js
@@ -22,6 +22,7 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import CloseIcon from "@material-ui/icons/Close";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { FormatAlignCenter } from "@material-ui/icons";
 const useStyles = makeStyles((theme) => ({
   root: {
     zIndex: 600,
@@ -172,7 +173,7 @@ const PhotoGallery = ({
           </IconButton>
         </DialogTitle>
         <Divider />
-        <Tabs
+        {/* <Tabs
           value={tabValue}
           onChange={(e, i) => {
             setTabValue(i);
@@ -187,7 +188,8 @@ const PhotoGallery = ({
           <Tab label="Inside" className={classes.tab} />
           <Tab label="360" className={classes.tab} />
           <Tab label="Streetview" className={classes.tab} />
-        </Tabs>
+    
+        </Tabs> */}
         <div className={classes.gallery}>
           <ImageList
             rowHeight={200}
