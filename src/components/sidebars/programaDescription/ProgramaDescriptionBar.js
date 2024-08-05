07deb9113b2468bbf6abe86d32ea988b42d61034
Lucas Tavares <lucastavarestt@gmail.com>
2024-08-05 14:29:07 -0300

    chore: added line brake to programa description

--- src/components/sidebars/programaDescription/ProgramaDescriptionBar.js
+++ src/components/sidebars/programaDescription/ProgramaDescriptionBar.js
@@ -389,9 +389,7 @@ const ProgramaDescriptionBar = forwardRef(
     const [isScreen500, setTextScreen500] = useState(false);
 
 
-    const fullText = programaData?.descricao?.replace(/\\r\\n/g, '<br />');
-    // const fullText = programaData?.descricao?.replace(/-/g, '<br />-').replace(/-/g, '<b>-</b>');
-    // .replace(/(\\r\\n)/g, '<br />')
+    const fullText = programaData?.descricao?.replace(/&lt;br \/&gt;\\r/g, '<br />').replace(/- /g, '• ')
 
     // Calcule o número de caracteres com base na altura da janela
     const numChars = Math.floor(windowHeight / (isScreen900 ? 3.9 : (isScreen500 ? 4 : 1.1)));
